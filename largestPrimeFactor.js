1dbebe3619e5da4e1921a54d9ba7a658469dbbeb
Mariel Salcedo <Mariel Salcedo>
2017-06-21 22:46:10 -1000

    assigned prime to t, called in function

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,20 @@
 exports.largestPrimeFactor = function(n){
-  var primeNumber = 0;
-  // do your work here
+var primeNumber = 0;
 
+  // do your work here
+  for ( let i = 2; i <= Math.sqrt(n); i++){
+    if ( n % i === 0 && isPrime(i)){
+      primeNumber = i;
+    }
+  }
   return primeNumber;
 };
+
+  function isPrime(n){
+    for( let i = 2; i < Math.sqrt(n); i++){
+      if (n % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
